4b217b6b05261e40892161f7b5277ded96f5e48d
keyiflerolsun <keyiflerolsun@gmail.com>
2025-06-16 02:21:04 +0300

    🕊
    🕊

--- ft_printf.c
+++ ft_printf.c
@@ -51,10 +51,7 @@ int	ft_printf(const char *format, ...)
 	while (*format)
 	{
 		if (*format != '%')
-		{
-			write(1, format++, 1);
-			steps++;
-		}
+			steps += ft_putchr(*format++);
 		else if (*(format + 1) == '%' && format++ && format++)
 			steps += ft_putchr('%');
 		else
